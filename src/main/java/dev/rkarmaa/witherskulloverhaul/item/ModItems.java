c3b627f0653bfc2ec15b6f2852b8f66286516712
Marco <luccimarco176@gmail.com>
2024-11-16 21:41:55 +0100

    REVERT TO v1.0.1

--- src/main/java/dev/rkarmaa/witherskulloverhaul/item/ModItems.java
+++ src/main/java/dev/rkarmaa/witherskulloverhaul/item/ModItems.java
@@ -1,11 +1,12 @@
 package dev.rkarmaa.witherskulloverhaul.item;
 
 import dev.rkarmaa.witherskulloverhaul.WitherSkullOverhaul;
+
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
+import net.minecraft.item.Item;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
